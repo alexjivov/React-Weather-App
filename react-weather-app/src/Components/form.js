8377fcffb39e9c44c501d2cc00f694f9d5a2b7ee
Alex Jivov <alexjivov@gwu.edu>
2018-08-25 20:34:10 -0400

    added city + country input form

--- react-weather-app/src/Components/form.js
+++ react-weather-app/src/Components/form.js
@@ -5,9 +5,12 @@ class Form extends Component {
     // Render - built in method that returns JSX - JS running in background transpiled by Babel
     render() {
       return (
-       <div>
-           Form Component
-        </div>
+        <form>
+            <input type="text" name="city" placeholder="City..."/>
+            <input type="text" name="country" placeholder="Country"/>
+            <button>Get Weather</button>
+            
+        </form>
       );
     }
   }
