a076a2e454e0c904cb7269fff129e399374e9069
Alex Jivov <alexjivov@gwu.edu>
2018-08-25 20:31:27 -0400

    added api call functions - not calling actual function yet

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -8,14 +8,29 @@ import './Assets/css/default.min.css';
 /////////////////
 import Titles from './Components/titles';
 import Form from './Components/form';
+import Weather from './Components/weather';
+
+// WEATHER API KEY //
+const API_key = "7d9bb4a9ebb655c852f28bb7d3c4081a";
+
 
 class App extends Component {
+  // Create your own method - Get Weather
+  // arrow functions allow you to use 'this' key word independently (its bound to this component)
+  getWeather = async () => {
+    // fetch keyword + URL you want to make API call to
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_key}a&units=metric`);
+    //need to convert response to JSON format
+    const data = await api_call.json();
+    console.log(data);
+    }
   // Render - built in method that returns JSX - JS running in background transpiled by Babel
   render() {
     return (
       <div>
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
---
+++ react-weather-app/src/Components/weather.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Weather extends Component {
+    // Render - built in method that returns JSX - JS running in background transpiled by Babel
+    render() {
+      return (
+       <div>
+           Weather Component
+        </div>
+      );
+    }
+  }
+  
+  export default Weather;
+  
